cfba7909a798feaf3aabd393423cb17b753eb5e5
dummy3k <l4711@gmx.net>
2010-05-03 22:28:43 +0200

    initial commit

---
+++ leopytools/helpers.py
@@ -0,0 +1,36 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+do_debug = True
+def ipython():
+    """ break and jump into ipython. call it this way:
+            h.debug()('foo')
+    """
+
+    if not do_debug:
+        def noop():
+            pass
+        return noop
+
+    from IPython.Shell import IPShellEmbed
+    # '-pdb',
+    args = ['-pi1', 'In <\\#>: ', '-pi2', '   .\\D.: ',
+            '-po', 'Out<\\#>: ', '-nosep']
+    ipshell = IPShellEmbed(args,
+        banner = 'Entering IPython.  Press Ctrl-D to exit. Set h.do_debug=False to never go here again.',
+        exit_msg = 'Leaving Interpreter, back to Pylons.')
+    return ipshell
+
+def iif(expr, a, b):
+    if expr:
+        return a
+    else:
+        return b
+
+def find(haystack, condition):
+    for x in haystack:
+        if condition(x):
+            return x
+
+    raise KeyError("condition did not match")
